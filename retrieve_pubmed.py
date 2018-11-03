0b2de7f214b5e905fb638f32a632461f7449b83a
anna-hernandez <31481762+anna-hernandez@users.noreply.github.com>
2018-11-03 12:00:02 +0000

    fixed query url in getPubmedSummary()

--- retrieve_pubmed.py
+++ retrieve_pubmed.py
@@ -15,9 +15,10 @@ def getPubmedSummary(pmidlist):
 
     mainurl = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
     esummary = 'esummary.fcgi?db=pubmed&retmode=json&id={}'.format(','.join(pmidlist))
-    data = requests.get(url)
+    #esearch = 'esearch.fcgi?db=pubmed&term=asthma&field=title'
+    data = requests.get(mainurl+esummary)
     parsed = json.loads(data.text)
-    
+    print(parsed)
     dct = {}
     
     for pmid in pmidlist:
